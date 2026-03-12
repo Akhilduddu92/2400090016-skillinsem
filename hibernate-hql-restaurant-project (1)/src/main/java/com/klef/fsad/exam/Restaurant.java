a5ed44743554d167ff5f3ceef2a2196425a2356d
Akhil Duddu <akhilduddu08@gmail.com>
2026-03-12 11:29:08 +0530

    Add files via upload

---
+++ hibernate-hql-restaurant-project (1)/src/main/java/com/klef/fsad/exam/Restaurant.java
@@ -0,0 +1,47 @@
+package com.klef.fsad.exam;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="restaurant")
+public class Restaurant
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    private String status;
+
+    private String location;
+
+    public Restaurant() {}
+
+    public Restaurant(String name, Date date, String status, String location)
+    {
+        this.name = name;
+        this.date = date;
+        this.status = status;
+        this.location = location;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Date getDate() { return date; }
+    public void setDate(Date date) { this.date = date; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+}
